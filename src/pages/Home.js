894945f0ac9220dcd79204b2a07a705d5941ccf0
Jeromescxz <Jeromehipolito14@gmail.com>
2021-05-18 10:11:47 +0800

    end of exam

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
 import Nav from "../components/Nav";
+import React, { useState } from "react";
+import "../App.css";
+
 function Home() {
   return (
     <div>
       <Nav />
+      <div className="logo">
+        <h1 id="vet_name">
+          {" "}
+          FUR PET {"\n"} VETERINARY {"\n"} CLINIC{" "}
+        </h1>
+        <h2 id="vet_tg"> Taking care of your pet is our passion. </h2>
+        <h4 id="vet_desc">
+          {" "}
+          Fur Pet Veterinary Clinic is 24/ 7 animal hospital that will serve
+          your pet.{" "}
+        </h4>
+      </div>
     </div>
   );
 }
